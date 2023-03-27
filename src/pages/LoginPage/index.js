44b47a7094907e97dbf9f03ea4ec6bc52fc133c8
Adamonis Alexandra <sasha987adamonis@gmail.com>
2023-03-27 16:31:54 +0300

    initial commit

---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './login.module.scss';
+
+function Login(props) {
+    return (
+        <div className={s.content}>
+            <h1>CONTOSO sales</h1>
+            <div className={s.login_area}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login;
