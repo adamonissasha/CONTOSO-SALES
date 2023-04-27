0a5148c9c0c3897111b5f7efc6e26745b98ce5a3
Adamonis Alexandra <sasha987adamonis@gmail.com>
2023-04-27 17:47:04 +0300

    add products page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import LoginPage from './pages/LoginPage';
 import AccountPage from './pages/AccountPage';
 import ManagersPage from './pages/ManagersPage';
+import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/managers" element={<ManagersPage />} />
+        <Route path="/products" element={<ProductsPage />} />
       </Routes>
     </div>
   );
