9e5a699d1bd1a638000da1ba01218b9ee391bdfc
Matvei Stremous <matvei.stremous@gmail.com>
2023-04-27 13:35:37 +0300

    add login request

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
-import AccountPage from './pages/AccountPage'
-import ManagersPage from './pages/ManagersPage';
+import AccountPage from './pages/AccountPage';
+// import ManagersPage from './pages/ManagersPage';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/account" element={<AccountPage />} />
-        <Route path="/managers" element={<ManagersPage />} />
+        {/* <Route path="/managers" element={<ManagersPage />} /> */}
       </Routes>
     </div>
   );
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import s from './login.module.scss';
+import AuthService from '../../services/AuthService';
 
 function Login() {
+    const [password, setPassword] = React.useState('admin');
+    const [login, setLogin] = React.useState('admin');
+
+    const onLogin = (e) => {
+        e.preventDefault();
+        const user = { login, password };
+        AuthService.login(user);
+    }
     return (
         <div className={s.content}>
             <h1>CONTOSO sales</h1>
@@ -10,19 +19,23 @@ function Login() {
                 <div className={s.input}>
                     <p>Имя пользователя</p>
                     <input
+                        value={login}
+                        onChange={(obj) => setLogin(obj.target.value)}
                         className={s.inp}
                         required />
                 </div>
                 <div className={s.input}>
                     <p>Пароль</p>
                     <input
+                        value={password}
+                        onChange={(obj) => setPassword(obj.target.value)}
                         className={s.inp}
                         required
                         type='password'
                         id='password_input'
                         name='password' />
                 </div>
-                <button className={s.but}>Войти</button>
+                <button className={s.but} onClick={(e) => onLogin(e)}>Войти</button>
             </div>
         </div>
     )
